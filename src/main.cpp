5feb88f0feff8719296069dd8bd4fc9041d3e591
mo-seph <github@mo-seph.com>
2021-06-15 21:31:52 +0200

    Updating to make working wiht more boards easy

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 //#include "FastLED.h"
 
 //#include "GlowStrip.h"
+#define FASTLED_ESP32_I2S true
 
 #include "FastLEDGlowStrip.h"
 //#include "GlowBehaviours.h"
@@ -20,23 +21,57 @@
 //#include "ArduinoJson.h"
 #include "FastLED_RGBW.h"
 #include "GlowMQTT.h"
+#include "NTPFeature.h"
+#include "WiFIFeature.h"
+#include "SerialConnector.h"
+
+//Need to include this here if we want it in the WiFiFeature
+#include <ArduinoOTA.h>
 
 
+#ifndef NUM_LEDS
+#define NUM_LEDS 100
+#endif
 
-#define NUM_LEDS 1200
-#define DATA_PIN 2
+#ifndef LED_DATA_PIN
+#define LED_DATA_PIN 12
+#endif
+
+#define LEDS_PER_STRIP 400
+#define NUM_STRIPS 3
 
 //#define STATE_LOGGING 1
 //#define TIME_LOGGING 1
 
+/* WIFI Defs */
 const char* ssid = "Fishbowl2G";
 const char* password = "29Goldfish";
 
-const char* mqtt_server = "192.168.178.123";
+/* MQTT Defs */
+const char* mqtt_server = "192.168.178.108";
 const int mqtt_port = 1883;
 
+/* NTP Defs */
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
 
+const char* id = "living";
+const char* name = "Living Room Lights";
 
+#define str(x) #x
+#define strname(name) str(name)
+
+#ifndef DEVICE_ID
+#define DEVICE_ID test
+#endif
+
+#ifndef DEVICE_NAME
+#define DEVICE_NAME Test Device
+#endif
+
+WiFiClient esp;
+PubSubClient client(esp);
 
 FRGBW current[NUM_LEDS];
 CRGBW leds[NUM_LEDS];
@@ -45,31 +80,24 @@ CRGB *ledsRGB = (CRGB *) &leds[0];
 const uint8_t brightness = 255;
 
 FastLEDGlowStrip strip(NUM_LEDS,leds, current);
+//GlowController glowControl(&strip,id,name);
+GlowController glowControl(&strip,strname(DEVICE_ID),strname(DEVICE_NAME));
 
+bool ota_running = false;
 
-//Fill base(&strip,FRGBW(0,0,0,0.0));
-
-float start = 9.0 / NUM_LEDS;
-float width = 9.0 / NUM_LEDS;
-float rate = 4.0 / NUM_LEDS;
-//Glow ball(&strip,AreaRGBW(FRGBW(1,0,0,0.0),start,width),rate);
-//Glow ball2(&strip,AreaRGBW(FRGBW(0,0,0,1.0),start+0.2,width),rate*1.1);
-//Watchdog watchdog(&strip,8);
-
-GlowController glowControl(&strip,true);
-
-
-float r = 0;
-
-long lastUpdate = millis();
-
-
-
-//char json[] =
-      //"{\"update\":0,\"data\":{\"r\":0.5,\"g\":0.1,\"b\":0.3,\"w\":0.7}}";
-//char baseJson[] =
-  //"{\"id\":0, \"type\":\"Fill\", \"name\":\"Background\", \"data\":{\"r\":0.5, \"g\":0.1, \"b\":0.3, \"w\":0.7} }";
-//
+/*
+  {
+    "id":2,
+    "type":"PixelClock",
+    "name":"Time"
+  },
+  {
+    "id":5,
+    "type":"Glow",
+    "name":"Red",
+    "data":{"r":0.8,"b":0.0,"g":0.0,"w":0.0,"rate":0.4}
+  },
+*/
 
 static const char fullJson[] PROGMEM = ( R"(
 {
@@ -78,24 +106,21 @@ static const char fullJson[] PROGMEM = ( R"(
     {
       "id":0,
       "type":"Fill",
-      "name":"Background",
+      "name":"Base",
       "data":{"r":0.0,"b":0.0,"g":0.0,"w":0.4}
     },
+
+
     {
-      "id":2,
-      "type":"PixelClock",
-      "name":"Time"
-    },
-    {
-      "id":5,
-      "type":"Glow",
-      "name":"Red",
-      "data":{"r":0.8,"b":0.0,"g":0.0,"w":0.0,"rate":0.4}
+      "id":6,
+      "type":"Alarm",
+      "name":"Alarms",
+      "data":{"start":0.01,"length":10,"time":20}
     },
     {
       "id":16,
       "type":"Watchdog",
-      "name":"Watchdog"
+      "name":"Chk"
     }
   ]
 })");
@@ -110,15 +135,29 @@ static const char fullJson[] PROGMEM = ( R"(
 
 
 void setup() {
+  glowControl.timeKeeping()->logState = 0;
+  glowControl.timeKeeping()->logTime = 0;
+
+
   Serial.begin(115200);
   while(!Serial && millis() < 4000 ) {}
   Serial.println("Starting out...");
 
-  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(ledsRGB, getRGBWsize(NUM_LEDS));
+  /* Serial version */
+  FastLED.addLeds<WS2812B, LED_DATA_PIN, RGB>(ledsRGB, getRGBWsize(NUM_LEDS));
+  /* Parallel version */
+  //FastLED.addLeds<WS2812B, 12, RGB>(ledsRGB, getRGBWsize(LEDS_PER_STRIP));
+  //FastLED.addLeds<WS2812B, 14, RGB>(ledsRGB + 4 * LEDS_PER_STRIP, getRGBWsize(LEDS_PER_STRIP));
+  //FastLED.addLeds<WS2812B, 15, RGB>(ledsRGB + 8 * LEDS_PER_STRIP, getRGBWsize(LEDS_PER_STRIP));
+
   FastLED.setBrightness(brightness);
   strip.show();
 
-  setupGlowMQTT(&glowControl, ssid, password, mqtt_server, mqtt_port);
+
+  glowControl.addFeature(new WiFiFeature(ssid,password));
+  glowControl.addFeature(new NTPFeature(ntpServer,gmtOffset_sec,daylightOffset_sec));
+  glowControl.addConnector(new SerialConnector());
+  glowControl.addConnector(new MQTTConnector(&client, mqtt_server, mqtt_port));
 
   glowControl.update(fullJson);
   Serial.println(F("Done loading behaviours"));
@@ -128,32 +167,20 @@ void setup() {
   Fill* base = (Fill*)glowControl.getBehaviour(0);
   base->fadeIn(startupColor,16);
   base->setInterpTime(.2);
-  glowControl.sendState();
+  //setupWiFi(ssid,password);
+  //glowControl.sendState();
+
 }
 
 void loop(){
-  #ifdef STATE_LOGGING
-  Serial.println("Loop...");
-  #endif
-
-  long t = millis();
-  mqttConnect();
-  glowControl.update();
-
-
-  #ifdef STATE_LOGGING
-  Serial.println("About to start behaviours loop...");
-  #endif
-
-  glowControl.runBehaviours();
-
-  #ifdef STATE_LOGGING
-  Serial.println("Done behaviours");
-  #endif
-
-  long update_t = millis() - t;
-  #ifdef TIME_LOGGING
-    Serial.print("Update time: ");Serial.print(update_t);
-    Serial.print(" FPS: ");Serial.println((int)(1000.0/update_t));
-  #endif
+  glowControl.loop();
+  /*
+  if( ! ota_running && WiFi.status() == WL_CONNECTED ) {
+    ota_running = true;
+    Serial.println("Starting OTA");
+    ArduinoOTA.setHostname(strname(DEVICE_ID));
+    ArduinoOTA.begin();
+  }
+  if(ota_running ) { ArduinoOTA.handle();}
+  */
 }
